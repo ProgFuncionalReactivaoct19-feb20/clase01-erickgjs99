8369d7cbf2fefb10ceff2c2d9762a37d835fe3ce
erickgjs99 <erickjs99lol@gmail.com>
2019-10-01 11:46:14 -0500

    hola

---
+++ ejemplo_1.py
@@ -0,0 +1,10 @@
+"""
+	autor: @erickgjs99
+	file: ejemplo_1.py
+"""
+valores = [10, 2 , 11, 12, 13]
+
+
+for l in valores:
+	if l%2==0:
+		print(l)
---
+++ ejemplo_2.py
@@ -0,0 +1,11 @@
+"""
+	autor: @erickgjs99
+	file: ejemplo_2.py
+"""
+
+
+valores = [10, 2 , 11, 12, 13]
+
+for l in valores:
+	if l%2==0:
+		print(l)
